55724bf1c27096ad51a6a9edeefd6b183def0fc3
Garrett Kegel <ghkegel@gmail.com>
2021-09-29 01:53:07 -0700

    Edited the index.js file in models to be correct.

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Portfolio = require('./Portfolio');
 
-User.hasMany(Project, {
+User.hasOne(Portfolio, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Portfolio.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Portfolio };
